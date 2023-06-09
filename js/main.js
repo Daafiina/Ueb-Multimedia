02c074d740dbff1749a5c85b3becd807a60fc171
mirjeta rexhepi <mr51033@ubt-uni.net>
2023-06-10 00:01:51 +0200

    Member and Reviews added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,47 @@
-jQuery(document).ready(function(){
+$(document).ready(function(){
     "user strick"
     $('.slider').ripples({
         dropRadius: 20,
         perturbance: 0.01,
       });
 
+      /*
       $(".text").typed({
         strings:["FGasdst Sentence.","Second Sentence"],
         typespeed:0,
         loop:true
+      });
+      */
+
+          
+
+      $('.work').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+        enabled: true
+        },        
+      });
+
+      $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 750,
+        loop: true,
+        autoplayHoverPause: true,
+        responsive:{
+        0:{
+          items: 1
+        },
+
+        480:{
+          items: 2
+        },
+
+        768:{
+          items:3
+        },
+      }
       });
 });
 
